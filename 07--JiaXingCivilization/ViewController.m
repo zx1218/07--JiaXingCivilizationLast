c4d61606d17d450b917911276ba939f3380bff61
dc007 <dc007@zhangxuan.local>
2016-06-22 13:10:00 +0800

    Initial Commit

---
+++ 07--JiaXingCivilization/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  07--JiaXingCivilization
+//
+//  Created by dc007 on 16/6/22.
+//  Copyright © 2016年 NSObject. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
